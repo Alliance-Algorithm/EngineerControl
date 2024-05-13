f9b0be586f0f18f2d925a63c5d82e904f64098e5
alray <1780284652@qq.com>
2024-05-13 12:57:55 +0000

    Deploy

--- .scripts/buildAndDeploy.sh
+++ .scripts/buildAndDeploy.sh
@@ -1,4 +1,11 @@
 source /opt/ros/humble/setup.zsh
-cd /workspaces/Engineer_Visual/src && colcon build --merge-install --build-base /workspaces/Engineer_Visual/deploy/build --install-base /workspaces/Engineer_Visual/deploy/install 
-unison -batch /workspaces/Engineer_Visual/deploy ssh://alliance@10.147.20.5//home/alliance/deploy
-ssh -t alliance@10.147.20.5 "cd deploy ; docker container stop deploy_yu_heng_n_n ; docker container rm deploy_yu_heng_n_n; docker run -d --name=deploy_yu_heng_n_n -v /dev:/dev --net=host --privileged --restart=always deploy_yu_heng_n_n;chmod +x ./YuHengArm/YuHengArm.x86_64;./YuHengArm/YuHengArm.x86_64"
+cd /workspaces/Engineer_Visual/src && colcon build --merge-install --build-base /workspaces/Engineer_Visual/deploy/d1/build --install-base /workspaces/Engineer_Visual/deploy/d1/install 
+unison -batch /workspaces/Engineer_Visual/deploy ssh://alliance@192.168.1.55//home/alliance/deploy
+ssh -t alliance@192.168.1.55 "cd deploy ; 
+ docker container stop deploy_yuheng_eye ; docker container rm deploy_yuheng_eye; 
+ docker run -d --name=deploy_yuheng_eye -v /dev:/dev --net=host --privileged --restart=always deploy_yuheng_eye;
+ docker container stop deploy_yuheng_arm ; docker container rm deploy_yuheng_arm; 
+ docker run -d --name=deploy_yuheng_arm -v /dev:/dev --net=host --privileged --restart=always deploy_yuheng_arm;
+ sudo cp ./RunAll.sh /usr/bin/RunAll.sh;sudo chmod +x /usr/bin/RunAll.sh;sudo cp ./RunAll.service /etc/systemd/system/RunAll.service;
+ sudo systemctl daemon-reload;sudo systemctl enable RunAll;sudo systemctl start RunAll
+"
--- .scripts/buildContainer.sh
+++ .scripts/buildContainer.sh
@@ -1,2 +1,7 @@
-unison -batch /workspaces/Engineer_Visual/deploy ssh://alliance@10.147.20.5//home/alliance/deploy
-ssh -t alliance@10.147.20.5 "cd deploy;sudo docker container stop deploy_yu_heng_n_n && sudo docker container rm deploy_yu_heng_n_n;sudo docker image rm deploy_yu_heng_n_n;sudo docker build -f Dockerfile -t deploy_yu_heng_n_n ."
+unison -batch /workspaces/Engineer_Visual/deploy ssh://alliance@192.168.1.55//home/alliance/deploy
+ssh -t alliance@192.168.1.55 "cd deploy/d1;
+sudo docker container stop deploy_yuheng_eye && sudo docker container rm deploy_yuheng_eye;
+sudo docker image rm deploy_yuheng_eye;sudo docker build --network=host -f Dockerfile -t deploy_yuheng_eye .;
+cd ../d2;
+sudo docker container stop deploy_yuheng_arm && sudo docker container rm deploy_yuheng_arm;
+sudo docker image rm deploy_yuheng_arm;sudo docker build --network=host -f Dockerfile -t deploy_yuheng_arm ."

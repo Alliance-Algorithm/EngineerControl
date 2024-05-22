b9ab24ed440ee22d309ee7555a5808fe6effe4f5
alray <1780284652@qq.com>
2024-05-22 18:53:30 +0000

    (closed)

--- deploy/RunAll.sh
+++ deploy/RunAll.sh
@@ -4,3 +4,5 @@ systemctl start docker
 docker restart deploy_yuheng_eye
 docker restart deploy_yuheng_arm
 source /opt/ros/humble/setup.sh
+
+docker run -v /dev:/dev --network host  --privileged qzhhhi/rmcs-runtime
